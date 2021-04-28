148b1689abde3b9351ed68264edde6dc67c7daae
epicurean21 <jm_company@naver.com>
2021-04-28 17:57:00 +0900

    21/04/28

--- 1068.cpp
+++ 1068.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <vector>
 #include <cstring>
@@ -47,5 +48,139 @@ int main() {
         if (exist[i] && tree[i].size() == 0)
             cnt++;
     cout << cnt << '\n';
+    return 0;
+}*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int cnt;
+
+class Node {
+public:
+    int data;
+    Node *par;
+    vector<Node *> child;
+
+    Node() {
+        data = 0;
+        par = NULL;
+    }
+};
+
+class Tree {
+public:
+    Node *root;
+    vector<Node *> list;
+
+    Tree() {
+        root = NULL;
+    }
+
+    Node *findNode(int e) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list[i]->data == e) {
+                return list[i];
+            }
+        }
+        return NULL;
+    }
+
+    void insertRoot() {
+        Node *new_node = new Node();
+        new_node->data = 0;
+        root = new_node;
+        list.push_back(new_node);
+    }
+
+    void insertNode(int p, int c) {
+        if (p == -1) {
+            insertRoot();
+        } else {
+            Node *par_node = findNode(p);
+
+            Node *new_node = new Node();
+            new_node->data = c;
+            new_node->par = par_node;
+            par_node->child.push_back(new_node);
+            list.push_back(new_node);
+        }
+    }
+
+    void delChild(Node *v) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list[i]->child.size(); j++) {
+                if (list[i]->child[j] == v) {
+                    list[i]->child[j]->par = NULL;
+                    list[i]->child[j] = NULL;
+                }
+            }
+        }
+    }
+
+    void del(Node *v) {
+        v->data = -1;
+        for (int i = 0; i < v->child.size(); i++) {
+            v->child[i]->par = NULL;
+            v->child[i] = NULL;
+        }
+        v = NULL;
+        return;
+    }
+
+    void eraseNode(int e) {
+        if (e == 0) {
+            return;
+        } else {
+            Node *del_node = findNode(e);
+            if (del_node == NULL) return;
+            del(del_node);
+            delChild(del_node);
+        }
+    }
+
+    int countChild(Node *v) {
+        if (v == NULL) return 0;
+        int count_child = 0;
+        for (int i = 0; i < v->child.size(); i++)
+            if (v->child[i] != NULL)
+                count_child++;
+        return count_child;
+    }
+
+    void leafCount(Node *v) {
+        for (int i = 0; i < list.size(); i++) {
+            int count_child= countChild(list[i]);
+            if (count_child == 0 && list[i]->data != -1)
+                cnt++;
+        }
+    }
+
+};
+
+int main() {
+
+    int N;
+    cin >> N;
+    Tree tree;
+
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        tree.insertNode(x, i);
+    }
+
+    int del;
+    cin >> del;
+
+    if (del == 0)
+        cout << 0 << endl;
+    else {
+        tree.eraseNode(del);
+        tree.leafCount(tree.root);
+        cout << cnt << endl;
+    }
     return 0;
 }
