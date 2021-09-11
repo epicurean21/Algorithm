1326f3f5e44ea621c303d9b5c03aa092c2735098
epicurean21 <jm_company@naver.com>
2021-09-11 18:00:30 +0900

    21/09/11 [Kakao #4: 브루트포스]

--- 2021_Kakao_Blind_4.cpp
+++ 2021_Kakao_Blind_4.cpp
@@ -4,17 +4,68 @@
 
 using namespace std;
 
-vector<int> solution(int n, vector<int> info) {
-    vector<int> answer;
+int score1, score2, ans;
+int arrow;
+int arr[11], arr2[11];
+vector<int> answer;
+bool flag = false;
 
+void sol(int i) {
+    if (i == 11) {
+        if (score2 - score1 > ans) {
+            flag = true;
+            ans = score2 - score1;
+            answer.clear();
+            for (int i = 0; i < 10; i++) {
+                answer.push_back(arr2[10 - i]);
+            }
+            answer.push_back(arrow);
+        }
+        return;
+    }
+    if (arr[i]) {
+        if (arrow >= arr[i] + 1) {
+            score2 += i;
+            arr2[i] = arr[i] + 1;
+            arrow -= arr2[i];
+            sol(i + 1);
+            score2 -= i;
+            arrow += arr2[i];
+            arr2[i] = 0;
+        }
+        arr2[i] = 0;
+        score1 += i;
+        sol(i + 1);
+        score1 -= i;
+    } else {
+        if (arrow > 0) {
+            arrow--;
+            arr2[i] = 1;
+            score2 += i;
+            sol(i + 1);
+            arrow++;
+            score2 -= i;
+        }
+        arr2[i] = 0;
+        sol(i + 1);
+    }
+}
+
+vector<int> solution(int n, vector<int> info) {
+    arrow = n;
+    for (int i = 0; i < 11; i++)
+        arr[10 - i] = info[i];
+    sol(1);
+    if (!flag) {
+        answer.push_back(-1);
+    }
     return answer;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    vector<int> info = {2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0};
-    solution(5, info);
+    vector<int> v = {0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3};
+    v = solution(10, v);
+    for (int x : v)
+        cout << x << " ";
 
-    return 0;
 }
