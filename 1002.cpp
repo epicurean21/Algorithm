fa2adfedcc3cd3190277c6002cb2ec6eb9a6d5b7
epicurean21 <jm_company@naver.com>
2021-09-11 17:21:46 +0900

    21/09/11 [1002 터렛: 수학, 기하학]

---
+++ 1002.cpp
@@ -0,0 +1,24 @@
+/**
+ * 1002: 터렛
+ * 수학, 기하학
+ * r1과 r2가 있을 때 류재명이 있을 수 있는 좌표 수
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int T, x_1, y_1, r_1, x_2, y_2, r_2; // (x1, y1): 조규현, (x2, y2): 백승찬
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> T;
+    while (T--) {
+        cin >> x_1 >> y_1 >> r_1 >> x_2 >> y_2 >> r_2;
+    }
+
+    return 0;
+}
