645e7db6df8d66001ba3ad9008f375e54d0bb786
epicurean21 <jm_company@naver.com>
2021-09-15 01:36:20 +0900

    21/09/15 [1520 내리막 길: 그래프 탐색, DP, DFS]

--- 1520.cpp
+++ 1520.cpp
@@ -1,32 +1,33 @@
 /**
  * 1520: 내리막 길
  * 그래프 탐색, DP, DFS
+ * 걍 4방향 탐색하면 시간초과임
+ * dp[i][j] = (i, j)는 N-1, M-1까지 dp[i][j]개 경로 저장
+ * 근데 dp값이 0일 수 있음 그래서 -1로 초기화해줘야함;; 이거 질문검색서 알게댐 ...
  */
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 #define MAX 502
-int N, M, map[MAX][MAX], dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1}, ans;
-bool chk[MAX][MAX];
+int N, M, map[MAX][MAX], dp[MAX][MAX], dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
 
-void dfs(int x, int y) {
-    chk[y][x] = true;
-    if (y == M - 1 && x == N - 1) {
-        ans++;
-        return;
-    }
+int dfs(int x, int y) {
+    if (y == M - 1 && x == N - 1)
+        return 1;
+    if (dp[y][x] != -1)
+        return dp[y][x];
+    dp[y][x] = 0;
 
     for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
-
         if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-        if (chk[ny][nx]) continue;
-        if (map[ny][nx] < map[y][x]) {
-            chk[ny][nx] = true;
-            dfs(ny, nx);
-        }
+        if (map[ny][nx] < map[y][x])
+            dp[y][x] += dfs(nx, ny);
     }
+
+    return dp[y][x];
 }
 
 int main() {
@@ -38,9 +39,8 @@ int main() {
         for (int j = 0; j < N; j++)
             cin >> map[i][j];
 
-    dfs(0, 0);
-
-    cout << ans << '\n';
+    memset(dp, -1, sizeof(dp));
+    cout << dfs(0, 0) << '\n';
 
     return 0;
 }
