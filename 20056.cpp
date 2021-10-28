cef182d9c8ec8b0c7d4084c46560384efcfd0b11
epicurean21 <jm_company@naver.com>
2021-10-28 17:51:18 +0900

    21/10/19 [20056 마법사 상어와 파이어볼: 구현, 시뮬레이션]

---
+++ 20056.cpp
@@ -0,0 +1,16 @@
+/**
+ * 20056: 마법사 상어와 파이어볼
+ * 삼성 S/W 검정 시험
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
--- cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/untitled.dir/1214.cpp.o"
+  "CMakeFiles/untitled.dir/20056.cpp.o"
   "untitled"
   "untitled.pdb"
 )
