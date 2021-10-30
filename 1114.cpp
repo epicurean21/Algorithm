1600ff6854587e71271e23a67d41fe56dfd756a3
epicurean21 <jm_company@naver.com>
2021-10-31 02:26:46 +0900

    21/10/31 [1114 통나무 자르기: 그리디 알고리즘, 이분탐색]

--- 1114.cpp
+++ 1114.cpp
@@ -3,12 +3,22 @@
  * 이분탐색, 그리디 알고리즘
  */
 #include <iostream>
-using namespace std;
+#include <vector>
 
-int main()
-{
+using namespace std;
+int L, K, C, cut;
+vector<int> cutable;
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+
+    cin >> L >> K >> C; // L 10억
+
+    for (int i = 0; i < K; i++) {
+        cin >> cut;
+        cutable.emplace_back(cut);
+    }
 
     return 0;
 }
