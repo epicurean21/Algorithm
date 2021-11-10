15eddebc847903bb55371c84831e81fe40c3ae2f
epicurean21 <jm_company@naver.com>
2021-11-10 19:23:46 +0900

    21/11/10 [5338 마이크로소프트 로고: 출력, 구현]

---
+++ 5338.cpp
@@ -0,0 +1,19 @@
+/**
+ * 5338 마이크로소프트 로고
+ * 구현
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "       _.-;;-._\n"
+            "'-..-'|   ||   |\n"
+            "'-..-'|_.-;;-._|\n"
+            "'-..-'|   ||   |\n"
+            "'-..-'|_.-''-._|" << "\n";
+    return 0;
+}
