23112ef29f5aefcac6f2838052f8b408544ffef7
epicurean21 <jm_company@naver.com>
2021-11-23 00:12:53 +0900

    21/11/23 [2104 부분배열 고르기: 세그먼트 트리, 부분합, 스택, 분할정복]

--- 2104.cpp
+++ 2104.cpp
@@ -1,97 +1,42 @@
-/**
- * 2104 부분배열 고르기
- * 세그먼트 트, 분할정복, 스택
- */
-
 #include <iostream>
-#include <stack>
-#include <vector>
-#include <cmath>
-#include <algorithm>
 
 using namespace std;
 #define ll long long
-int N;
-vector<int> tree_min, A;
-vector<ll> tree_sum;
+#define MAX 100001
+int n, arr[MAX];
 
+ll DnC(int s, int e) {
+    int l, r, height;
+    ll base;
+    if (s == e)
+        return (ll) arr[s] * arr[s];
+    int mid = (s + e) / 2;
+    ll m = max(DnC(s, mid), DnC(mid + 1, e));
+    l = mid;
+    r = mid + 1;
+    base = arr[l] + arr[r];
+    height = min(arr[l], arr[r]);
+    m = max(m, base * height);
 
-int cal_index(int a, int b) {
-    if (a == -1)
-        return b;
-    if (b == -1)
-        return a;
-    if (A[a] < A[b]) return a;
-    return b;
-}
-
-int min_value(int node, int start, int end, int left, int right) {
-    if (start > right || end < left) return INT32_MAX;
-
-    if (start >= left && end <= right)
-        return tree_min[node];
-
-    int m = (start + end) / 2;
-    return cal_index(min_value(node * 2, start, m, left, right), min_value(node * 2 + 1, m + 1, end, left, right));
-}
-
-ll sum_value(int node, int start, int end, int left, int right) {
-    if (start > right || end < left)
-        return 0;
-
-    if (start >= left && end <= right)
-        return tree_sum[node];
-
-    int m = (start + end) / 2;
-    return sum_value(node * 2, start, m, left, right) + sum_value(node * 2 + 1, m + 1, end, left, right);
-}
-
-ll construct_tree_sum(int node, int start, int end) {
-    if (start == end)
-        return tree_sum[node] = (ll) A[start];
-
-    int m = (start + end) / 2;
-
-    return tree_sum[node] = construct_tree_sum(node * 2, start, m) + construct_tree_sum(node * 2 + 1, m + 1, end);
-}
-
-int construct_tree_min(int node, int start, int end) {
-    if (start == end)
-        return tree_min[node] = A[start];
-    int m = (start + end) / 2;
-    return tree_min[node] = cal_index(construct_tree_min(node * 2, start, m),
-                                      construct_tree_min(node * 2 + 1, m + 1, end));
-}
-
-ll query(int start, int end) {
-    if (start == end)
-        return tree_sum[start] * tree_min[start];
-    int idx = min_value(1, 1, N, start, end);
-    ll ans = A[idx] * sum_value(1, 1, N, start, end);
-    if (start < idx)
-        ans = max(ans, query(start, idx - 1));
-    if (idx < end)
-        ans = max(ans, query(idx + 1, end));
-
-    return ans;
+    while (!(l == s && r == e)) {
+        if (r < e && (l == s || arr[l - 1] < arr[r + 1])) {
+            base += arr[++r];
+            height = min(height, arr[r]);
+        } else {
+            base += arr[--l];
+            height = min(height, arr[l]);
+        }
+        m = max(m, base * height);
+    }
+    return m;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
-    cin >> N;
-    int h = (int) ceil(log2(N + 1));
-    A.resize(N + 1);
-    tree_sum.resize(1 << (h + 1));
-    tree_min.resize(1 << (h + 1));
-
-    for (int i = 1; i <= N; i++)
-        cin >> A[i];
-
-    construct_tree_min(1, 1, N);
-    construct_tree_sum(1, 1, N);
-
-    cout << query(1, N) << '\n';
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << DnC(0, n - 1) << '\n';
     return 0;
 }
