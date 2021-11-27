1d6280dd500f7cdcbdb1f3fa73059fab077c52bb
epicurean21 <jm_company@naver.com>
2021-11-27 21:15:13 +0900

    21/11/27 [1365 꼬인 전깃줄: 이분탐색, 가장 긴 증가하는 부분 수열]

--- 1365.cpp
+++ 1365.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
-
-int N, input;
+#define MAX 100001
+int N, input, lcs[MAX], len = -1;
 vector<int> arr;
+
+void lower_bound(int n) {
+    int tmp = arr[n];
+
+    for (int i = 0; i <= len; i++) {
+        if (tmp < lcs[i]) {
+            lcs[i] = tmp;
+            break;
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> N;
-    arr.emplace_back(-1);
+
     for (int i = 0; i < N; i++) {
         cin >> input;
-        if (input > arr[arr.size() - 1])
-            arr.emplace_back(input);
-        else
-            arr[lower_bound()
+        arr.emplace_back(input);
     }
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (len == -1)
+            lcs[++len] = arr[i];
+        else if (lcs[len] <= arr[i])
+            lcs[++len] = arr[i];
+        else if (lcs[len] > arr[i])
+            lower_bound(i);
+    }
+
+    cout << N - (len + 1) << '\n';
 
     return 0;
 }
