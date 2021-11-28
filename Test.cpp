ec271508482060b007ee43a9a786f4bfa6529e99
epicurean21 <jm_company@naver.com>
2021-11-28 22:09:27 +0900

    21/11/28 [3015 오아시스 재결합: 스택]

--- Test.cpp
+++ Test.cpp
@@ -1,89 +1,35 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
-#define MAX 102
-int T, N;
-char X;
-int Failure_Function[MAX];
-string P, tmp; // Text, Pattern
-
-void calculate_failure_function(int n) {
-    Failure_Function[0] = 0; // Failure Function 0 은 항상 0
-    int i = 1, j = 0;
-    while (i < n) {
-        if (P[i] == P[j]) { // Match !
-            Failure_Function[i] = ++j; // F[i] = j + 1;
-            i++; // i++;
-        } else if (j > 0) { // j > 0 인데 mismatch인 경우, 즉 앞에서 몇 문자는 Match했음.
-            j = Failure_Function[j - 1];
-        } else { // mismatch 이면서, j = 0, Brute Force와 같이 첫 문자부터 틀렸기에 한 칸 shift
-            Failure_Function[j] = 0;
-            i++;
-        }
-    }
-}
-
-vector<int> KMP(int n, int m) {
-    int i = 0, j = 0;
-    vector<int> matching_index;
-
-    while (i < n) {
-        if (tmp[i] == P[j]) {
-            if (j == m - 1) {
-                matching_index.emplace_back(i - j);
-                j = 0;
-                continue;
-            }
-        } else if (j > 0)
-            j = Failure_Function[j - 1];
-        else
-            i++;
-    }
-
-    return matching_index;
-}
+int N, height, max_height, ans;
+stack<int> st;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> T;
-    while (T--) {
-        cin >> N;
-        P = "";
-        tmp = "";
-        for (int i = 0; i < N; i++) {
-            cin >> X;
-            P += X;
-        }
+    cin >> N;
+    ans = N - 1;
 
-        int length = 0;
-        vector<int> ans;
-        for (int i = 0; i < N; i++) {
-            tmp += P[i];
-            calculate_failure_function(tmp.length());
-            vector<int> kmp_answer = KMP(P.length(), tmp.length());
-            if (kmp_answer.size() > length)
-                ans = kmp_answer;
-        }
+    for (int i = 0; i < N; i++) {
+        cin >> height;
 
-        for (auto i : ans)
-            cout << i << " ";
-        cout << "\n";
+        if (st.empty()) {
+            st.push(height);
+            max_height = height;
+        } else {
+            if (height >= max_height) {
+                ans++;
+                max_height = height;
+            } else {
+                st.pop();
+                st.push(max_height);
+                max_height = height;
+            }
+        }
     }
 
+    cout << ans << '\n';
     return 0;
 }
-
-/**
-4
-10
-5 2 1 5 5 2 1 5 5 2
-10
-1 2 3 4 5 6 7 1 2 3
-10
-1 2 3 4 5 6 7 1 2 2
-15
-1 2 1 2 1 2 1 2 1 2 1 2 1 2 1
-*/
--- cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/jaemincho/Desktop/JaeMinCho/Algorithm/C,C++/17845.cpp" "/Users/jaemincho/Desktop/JaeMinCho/Algorithm/C,C++/cmake-build-debug/CMakeFiles/untitled.dir/17845.cpp.o"
+  "/Users/jaemincho/Desktop/JaeMinCho/Algorithm/C,C++/Test.cpp" "/Users/jaemincho/Desktop/JaeMinCho/Algorithm/C,C++/cmake-build-debug/CMakeFiles/untitled.dir/Test.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
