74220a612669ea396baa26c980127163a8fd05ed
epicurean21 <jm_company@naver.com>
2021-11-29 18:41:14 +0900

    21/11/29 [3015 불켜기: BFS, 그래프 이론]

--- 3015.cpp
+++ 3015.cpp
@@ -1,10 +1,47 @@
+/**
+ * 3015 오아시스 재결합
+ * 스택, 수학
+ */
+
 #include <iostream>
+#include <stack>
 
 using namespace std;
+int N, height, max_height = INT32_MAX, ans;
+stack<int> st;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> height;
+
+        if (st.empty() && (height <= max_height)) {
+            st.push(height);
+            max_height = st.top();
+        } else {
+            while (st.top() >= height) {
+
+            }
+        }
+        if (st.empty()) {
+            st.push(height);
+            max_height = height;
+        } else {
+            if (height >= max_height) {
+                ans++;
+                max_height = height;
+            } else {
+                st.pop();
+                st.push(max_height);
+                max_height = height;
+            }
+        }
+    }
+
+    cout << ans << '\n';
     return 0;
 }
