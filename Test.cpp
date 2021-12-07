07525e6d9b59b9c4560713605b87ba0ed9cfefb8
epicurean21 <jm_company@naver.com>
2021-12-07 21:56:51 +0900

    21/12/07 [23801 두 단계 최단 경로 2 : 다익스트라, 그래프 이론]

--- Test.cpp
+++ Test.cpp
@@ -1,122 +1,66 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <cmath>
+#include <map>
 
 using namespace std;
 #define ll long long
-int N, M, P, L, R, X;
-vector<pair<ll, ll>> tree, lazy;
-vector<int> An;
+int N, M, u, v, w, X, Z, P;
+map<int, int> p;
+vector<ll> dist;
+vector<vector<pair<int, int>>> road;
 
-ll construct_tree_sum(int node, int start, int end) {
-    if (start == end)
-        return tree[node].first = An[start];
-
-    int m = (start + end) / 2;
-    return tree[node].first = construct_tree_sum(node * 2, start, m)
-                              + construct_tree_sum(node * 2 + 1, m + 1, end);
+void init() {
+    for (int i = 1; i <= N; i++)
+        dist[i] = INT64_MAX;
 }
 
-ll construct_tree_max(int node, int start, int end) {
-    if (start == end)
-        return tree[node].second = An[start];
+void dijkstra(int start, int end) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq_for_second;
+    pq.push({start, 0});
+    dist[start] = 0;
 
-    int m = (start + end) / 2;
-    return tree[node].second = max(construct_tree_max(node * 2, start, m),
-                                   construct_tree_max(node * 2 + 1, m + 1, end));
-}
+    while (!pq.empty()) {
+        int cur = pq.top().first;
+        int weight = pq.top().second;
 
-void update_lazy_sum(int node, int start, int end) {
-    if (lazy[node].first == 0)
-        return;
+        if (dist[cur] < weight) continue;
 
-    tree[node].first += (end - start + 1) * lazy[node].first;
+        for (auto &i : road[cur]) {
+            int next = i.first;
+            int next_weight = i.second;
 
-    if (start != end) {
-        lazy[node * 2].first += lazy[node].first;
-        lazy[node * 2 + 1].first += lazy[node].first;
+            if (dist[next] > weight + next_weight) {
+                dist[next] = weight + next_weight;
+                pq.push({next, weight + next_weight});
+            }
+        }
     }
-
-    lazy[node].first = 0;
-}
-
-void update_lazy_max(int node, int start, int end) {
-    if (lazy[node].second == 0)
-        return;
-
-    tree[node].second += (end - start + 1) * lazy[node].second;
-
-    if (start != end) {
-        lazy[node * 2].second += lazy[node].second;
-        lazy[node * 2 + 1].second += lazy[node].second;
-    }
-
-    lazy[node].second = 0;
-}
-
-void min_update(int node, int start, int end, int left, int right, int diff) {
-    update_lazy_sum(node, start, end);
-    update_lazy_max(node, start, end);
-
-    if (start > right || end < left)
-        return;
-
-    if (start >= left && end <= right) {
-
-    }
-}
-
-ll sum(int node, int start, int end, int left, int right) {
-    if (start > right || end < left)
-        return 0;
-
-    if (start >= left && end <= right)
-        return tree[node].first;
-
-    int m = (start + end) / 2;
-    return sum(node * 2, start, m, left, right) + sum(node * 2 + 1, m + 1, end, left, right);
-}
-
-ll _max(int node, int start, int end, int left, int right) {
-    if (start > right || end < left)
-        return 0;
-
-    if (start >= left && end <= right)
-        return tree[node].second;
-
-    int m = (start + end) / 2;
-    return max(_max(node * 2, start, m, left, right), _max(node * 2 + 1, m + 1, end, left, right));
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N;
-    int height = (int) ceil(log2(N + 1));
-    tree.resize(1 << (height + 1));
-    lazy.resize(1 << (height + 1));
-    An.resize(N + 1);
-    for (int i = 1; i <= N; i++)
-        cin >> An[i];
-
-    construct_tree_sum(1, 1, N);
-    construct_tree_max(1, 1, N);
-
-    cin >> M;
-    while (M--) {
-        cin >> P;
-        if (P == 1) { // min값 적용 X에
-            cin >> L >> R >> X;
-            min_update(1, 1, N, L, R, X);
-        } else if (P == 2) {
-            cin >> L >> R;
-            cout << _max(1, 1, N, L, R) << '\n';
-        } else if (P == 3) {
-            cin >> L >> R;
-            cout << sum(1, 1, N, L, R) << '\n';
-        }
+    cin >> N >> M;
+    road.resize(N + 1);
+    dist.resize(N + 1);
+    for (int i = 0; i < M; i++) {
+        cin >> u >> v >> w;
+        road[u].emplace_back(v, w);
+        road[v].emplace_back(u, w);
     }
+
+    cin >> X >> Z >> P;
+
+    for (int i = 0; i < P; i++) {
+        cin >> v;
+        p.insert({i, v});
+    }
+
+    init();
+
 
     return 0;
 }
