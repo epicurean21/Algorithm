d4572e140eb8c0091b77715d3a36613fd91d0453
epicurean21 <jm_company@naver.com>
2021-12-07 22:27:29 +0900

    21/12/07 [23801 두 단계 최단 경로 2 : 다익스트라, 그래프 이론]

--- 23801.cpp
+++ 23801.cpp
@@ -1,4 +1,104 @@
-//
-// Created by 조재민 on 2021/12/07.
-//
+/**
+ * 23801 두 단계 최단 경로 2
+ * 다익스트라, 그래프 이론
+ * 두 번의 다익스트라를 돌리면 댄다
+ */
 
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <map>
+
+using namespace std;
+#define ll long long
+int N, M, u, v, w, X, Z, P;
+map<int, int> p;
+vector<ll> dist, dist2;
+vector<vector<pair<int, int>>> road;
+
+void init() {
+    for (int i = 1; i <= N; i++) {
+        dist[i] = INT64_MAX;
+        dist2[i] = INT64_MAX;
+    }
+}
+
+void dijkstra(int start) {
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq;
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq_for_second;
+    pq.push({0, start});
+    dist[start] = 0;
+
+    while (!pq.empty()) {
+        int cur = pq.top().second;
+        ll weight = pq.top().first;
+        pq.pop();
+
+        if (dist[cur] < weight) continue;
+
+        for (auto &i : road[cur]) {
+            int next = i.first;
+            ll next_weight = i.second + weight;
+
+            if (dist[next] > next_weight) {
+                dist[next] = next_weight;
+                pq.push({next_weight, next});
+                if (p.count(next)) {
+                    pq_for_second.push({dist[next], next});
+                }
+            }
+        }
+    }
+
+    if (pq_for_second.empty()) return;
+
+    dist2[pq_for_second.top().second] = pq_for_second.top().first;
+
+    while (!pq_for_second.empty()) {
+        int cur = pq_for_second.top().second;
+        ll weight = pq_for_second.top().first;
+        pq_for_second.pop();
+
+        if (dist2[cur] < weight) continue;
+
+        for (auto &i : road[cur]) {
+            int next = i.first;
+            ll next_weight = i.second + weight;
+
+            if (dist2[next] > next_weight) {
+                dist2[next] = next_weight;
+                pq_for_second.push({next_weight, next});
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+    road.resize(N + 1);
+    dist.resize(N + 1);
+    dist2.resize(N + 1);
+
+    for (int i = 0; i < M; i++) {
+        cin >> u >> v >> w;
+        road[u].emplace_back(v, w);
+        road[v].emplace_back(u, w);
+    }
+
+    cin >> X >> Z >> P;
+
+    for (int i = 0; i < P; i++) {
+        cin >> v;
+        p.insert({v, i});
+    }
+
+    init();
+    dijkstra(X);
+    if (dist2[Z] == INT64_MAX) cout << "-1\n";
+    else cout << dist2[Z] << '\n';
+
+    return 0;
+}
--- Test.cpp
+++ Test.cpp
@@ -7,33 +7,61 @@ using namespace std;
 #define ll long long
 int N, M, u, v, w, X, Z, P;
 map<int, int> p;
-vector<ll> dist;
+vector<ll> dist, dist2;
 vector<vector<pair<int, int>>> road;
 
 void init() {
-    for (int i = 1; i <= N; i++)
+    for (int i = 1; i <= N; i++) {
         dist[i] = INT64_MAX;
+        dist2[i] = INT64_MAX;
+    }
 }
 
-void dijkstra(int start, int end) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq_for_second;
-    pq.push({start, 0});
+void dijkstra(int start) {
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq;
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq_for_second;
+    pq.push({0, start});
     dist[start] = 0;
 
     while (!pq.empty()) {
-        int cur = pq.top().first;
-        int weight = pq.top().second;
+        int cur = pq.top().second;
+        ll weight = pq.top().first;
+        pq.pop();
 
         if (dist[cur] < weight) continue;
 
         for (auto &i : road[cur]) {
             int next = i.first;
-            int next_weight = i.second;
+            ll next_weight = i.second + weight;
 
-            if (dist[next] > weight + next_weight) {
-                dist[next] = weight + next_weight;
-                pq.push({next, weight + next_weight});
+            if (dist[next] > next_weight) {
+                dist[next] = next_weight;
+                pq.push({next_weight, next});
+                if (p.count(next)) {
+                    pq_for_second.push({dist[next], next});
+                }
+            }
+        }
+    }
+
+    if (pq_for_second.empty()) return;
+
+    dist2[pq_for_second.top().second] = pq_for_second.top().first;
+
+    while (!pq_for_second.empty()) {
+        int cur = pq_for_second.top().second;
+        ll weight = pq_for_second.top().first;
+        pq_for_second.pop();
+
+        if (dist2[cur] < weight) continue;
+
+        for (auto &i : road[cur]) {
+            int next = i.first;
+            ll next_weight = i.second + weight;
+
+            if (dist2[next] > next_weight) {
+                dist2[next] = next_weight;
+                pq_for_second.push({next_weight, next});
             }
         }
     }
@@ -46,6 +74,8 @@ int main() {
     cin >> N >> M;
     road.resize(N + 1);
     dist.resize(N + 1);
+    dist2.resize(N + 1);
+
     for (int i = 0; i < M; i++) {
         cin >> u >> v >> w;
         road[u].emplace_back(v, w);
@@ -56,11 +86,13 @@ int main() {
 
     for (int i = 0; i < P; i++) {
         cin >> v;
-        p.insert({i, v});
+        p.insert({v, i});
     }
 
     init();
-
+    dijkstra(X);
+    if (dist2[Z] == INT64_MAX) cout << "-1\n";
+    else cout << dist2[Z] << '\n';
 
     return 0;
 }
