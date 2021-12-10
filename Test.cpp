d9a853e897783db56ed9268cbea60ad9f57d589f
epicurean21 <jm_company@naver.com>
2021-12-11 00:03:45 +0900

    21/12/10 [13547 수열과 쿼리5 : 오프라인 쿼리, mo's]

--- Test.cpp
+++ Test.cpp
@@ -1,81 +1,63 @@
-/**
- * 13547 수열과 쿼리 5 [플래티넘 2]
- * 오프라인 쿼리 & mo's
-*/
-
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <cmath>
 
 using namespace std;
-#define MAX 1000001
-int N, M, A[MAX], table[MAX], X, Y, sqrt_value;
-vector<pair<int, pair<int, int>>> query;
-vector<int> res;
+#define MAX 100001
+int n, m, p, q;
+int A[MAX], cnt[MAX * 10], query[MAX];
+struct Query {
+    int idx, s, e;
+} Q[MAX];
+int sq;
 
-bool cmp(pair<int, pair<int, int>> &x, pair<int, pair<int, int>> &y) {
-    if (x.second.first / sqrt_value != y.second.first / sqrt_value)
-        return x.second.first / sqrt_value < y.second.first / sqrt_value;
-    return x.second.second < y.second.second;
+bool cmp(Query u, Query v) {
+    if (u.s / sq == v.s / sq) return u.e < v.e;
+    return u.s / sq < v.s / sq;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin >> N;
-    sqrt_value = sqrt(N);
-    for (int i = 0; i < N; i++)
-        cin >> A[i];
-    cin >> M;
-    query.resize(M + 1);
-    res.resize(M + 1);
-
-    for (int i = 0; i < M; i++) {
-        cin >> X >> Y;
-        query[i] = {i, {X - 1, Y - 1}};
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    sq = sqrt(n);
+    for (int i = 0; i < n; i++) cin >> A[i];
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> p >> q;
+        Q[i] = {i, p - 1, q - 1};
     }
-
-    sort(query.begin(), query.end(), cmp);
-
-    int ans = 0;
-    int s = query[0].second.first, e = query[0].second.second;
-    for (int a = s; a <= e; a++) {
-        if (table[A[a]] == 0)
-            ans++;
-        table[A[a]]++;
+    sort(Q, Q + m, cmp);
+    int ret = 0;
+    int st = Q[0].s;
+    int ed = Q[0].e;
+    for (int i = st; i <= ed; i++) {
+        if (cnt[A[i]]++ == 0) ret++;
     }
+    query[Q[0].idx] = ret;
 
-    res[query[0].first] = ans;
-
-    for (int a = 1; a < M; a++) {
-        while (query[a].second.first < s) {
-            if (table[A[--s]] == 0)
-                ans++;
-            table[A[s]]++;
-        }
-        while (query[a].second.second > e) {
-            if (table[A[++e]] == 0)
-                ans++;
-            table[A[e]]++;
-        }
-        while (query[a].second.first > s) {
-            table[A[s]]--;
-            if (table[A[s++]] == 0)
-                ans--;
-        }
-        while (query[a].second.second < e) {
-            table[A[e]]--;
-            if (table[A[e--]] == 0)
-                ans--;
+    for (int i = 1; i < m; i++) {
+        while (Q[i].s < st) {
+            if (cnt[A[--st]]++ == 0)
+                ret++;
         }
 
-        res[query[a].first] = ans;
+        while (Q[i].e > ed) {
+            if (cnt[A[++ed]]++ == 0)
+                ret++;
+        }
+
+        while (Q[i].s > st) {
+            if (--cnt[A[st++]] == 0) ret--;
+        }
+
+        while (Q[i].e < ed) {
+            if (--cnt[A[ed--]] == 0) ret--;
+        }
+
+        query[Q[i].idx] = ret;
     }
-
-    for (int i = 0; i < M; i++)
-        cout << res[i] << '\n';
-
-    return 0;
+    for (int i = 0; i < m; i++) {
+        cout << query[i] << '\n';
+    }
 }
