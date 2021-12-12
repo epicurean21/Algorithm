cc0ee4d35271999b423d022224fd8d8746892c1e
epicurean21 <jm_company@naver.com>
2021-12-12 16:10:39 +0900

    21/12/12 [3584 트리와 쿼리 2: LCA, DFS, 최소 공통 조상]

--- 3584.cpp
+++ 3584.cpp
@@ -1,99 +1,146 @@
 /**
- * 3584 가장 가까운 공통 조상
- * LCA, 그래프 이론, DFS
+ * 13511 트리와 쿼리 2 [플레티넘 2]
+ * LCA, 공통조상찾기
+ * 어렵네.. 해결못했음
  */
+
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <cmath>
-#include <cstring>
+#include <algorithm>
 
 using namespace std;
-#define MAX 10001
-int T, N, A, B, ancestor[MAX][20], X, Y;
-vector<vector<int>> tree;
-int depth[MAX], p[MAX];
-int max_level;
+#define MAX 100001
+#define ll long long
+int N, M, u, v, w, k, q;
+int depth[MAX], ancestor[MAX][20], p[MAX];
+ll cost_tree[MAX][20];
+bool visited[MAX];
+vector<vector<pair<int, int>>> tree(MAX);
 
-void init(int n) {
-    tree.clear();
-    tree.resize(n + 1);
-    memset(depth, 0, sizeof(depth));
-    memset(p, 0, sizeof(p));
-
-    for (int i = 0; i <= n; i++)
-        for (int j = 0; j < 20; j++)
-            ancestor[i][j] = 0;
-}
-
-void dfs(int here, int parent) {
-    depth[here] = depth[parent] + 1;
-    ancestor[here][0] = parent;
-    max_level = (int) floor(log2(MAX));
-
-    for (int i = 1; i <= max_level; i++) {
-        int tmp = ancestor[here][i - 1];
-        ancestor[here][i] = ancestor[tmp][i - 1];
-    }
-
-    for (int i = 0; i < tree[here].size(); i++) {
-        int there = tree[here][i];
-        if (there != parent)
-            dfs(there, here);
+void dfs(int p, int c, int dep, int cost) {
+    if (visited[c]) return;
+    visited[c] = true;
+    depth[c] = dep;
+    ancestor[c][0] = p;
+    cost_tree[c][0] = cost;
+    for (int i = 0; i < tree[c].size(); i++) {
+        dfs(c, tree[c][i].first, dep + 1, tree[c][i].second);
     }
 }
 
-int lca(int a, int b) {
+ll query_one_lca(int a, int b) {
+    ll cost = 0;
+    if (depth[a] < depth[b]) {
+        swap(a, b);
+    }
+
     if (depth[a] != depth[b]) {
-        if (depth[a] > depth[b])
-            swap(a, b);
-
-        for (int i = max_level; i >= 0; i--) {
-            if (depth[a] <= depth[ancestor[b][i]])
-                b = ancestor[b][i];
+        int diff = depth[a] - depth[b];
+        for (int i = 19; i >= 0; i--) {
+            if (diff >= (1 << i)) {
+                cost += cost_tree[a][i];
+                a = ancestor[a][i];
+            }
         }
     }
 
-    int lca = a;
+    if (a == b)
+        return cost;
+
+    for (int i = 19; i >= 0; i--) {
+        if (ancestor[a][i] != ancestor[b][i]) {
+            cost += cost_tree[a][i];
+            a = ancestor[a][i];
+            cost += cost_tree[b][i];
+            b = ancestor[b][i];
+        }
+    }
+
+    return cost + cost_tree[a][0] + cost_tree[b][0];
+}
+
+int query_two_lca(int a, int b, int c) {
+    if (depth[a] < depth[b]) {
+        swap(a, b);
+    }
+
+    if (depth[a] != depth[b]) {
+        int diff = depth[a] - depth[b];
+        for (int i = 19; i >= 0; i--) {
+            if (diff >= (1 << i)) {
+                a = ancestor[a][i];
+            }
+        }
+    }
+
     if (a != b) {
-        for (int i = max_level; i >= 0; i--) {
+        for (int i = 19; i >= 0; i--) {
             if (ancestor[a][i] != ancestor[b][i]) {
                 a = ancestor[a][i];
                 b = ancestor[b][i];
             }
-            lca = ancestor[a][i];
         }
     }
 
-    return lca;
+    return ancestor[a][0];
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> T;
-    while (T--) {
-        cin >> N;
-        init(N);
-        for (int i = 0; i < N - 1; i++) {
-            cin >> A >> B;
-            tree[A].emplace_back(B);
-            tree[B].emplace_back(A);
-            p[B] = A;
-        }
-        int root = 0;
-        for (int i = 1; i <= N; i++)
-            if (p[i] == 0)
-                root = i;
-
-            depth[0] = -1;
-
-            dfs(root, 0);
-
-            cin >> X >> Y;
-
-            cout << lca(X, Y) << '\n';
+    cin >> N;
+    for (int i = 0; i < N - 1; i++) {
+        cin >> u >> v >> w;
+        tree[u].emplace_back(v, w);
+        tree[v].emplace_back(u, w);
+        p[v] = u;
     }
+
+    int root = -1;
+    for (int i = 1; i <= N; i++) {
+        if (p[i] == 0) {
+            root = i;
+            break;
+        }
+    }
+
+    dfs(0, root, 0, 0);
+    cin >> M;
+    while (M--) {
+        cin >> q >> u >> v;
+        if (q == 1) {
+            cout << query_one_lca(u, v) << '\n';
+        } else if (q == 2) {
+            cin >> k;
+            int p = query_two_lca(u, v, k);
+            if (depth[u] - depth[v] + 1 >= k) {
+                int diff = k - 1;
+                int cur = u;
+
+                for (int i = 0; diff != 0; ++i) {
+                    if (diff % 2 == 1)
+                        cur = ancestor[cur][i];
+                    diff = diff >> 1;
+                }
+
+                cout << cur << '\n';
+            } else {
+                k = k - (depth[u] - depth[p]);
+
+                int diff = depth[v] - depth[p] + 1 - k;
+                int cur = v;
+
+                for (int i = 0; diff != 0; ++i) {
+                    if (diff % 2 == 1)
+                        cur = ancestor[cur][i];
+                    diff = diff >> 1;
+                }
+
+                cout << cur << '\n';
+            }
+        }
+    }
+
     return 0;
 }
