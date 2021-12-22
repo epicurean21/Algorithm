102aa8c7f0402c80891235133da6e63295e140fd
epicurean21 <jm_company@naver.com>
2021-12-23 00:23:46 +0900

    21/12/23 [17144 미세먼지 안녕! : 구현, 시뮬레이션]

--- Test.cpp
+++ Test.cpp
@@ -2,73 +2,83 @@
  * 17144 미세먼지 안녕!
  * 구현, 시뮬레이션
  */
-#include <iostream>
-#include <cstring>
+#include<iostream>
 
-using namespace std;
 #define MAX 51
+using namespace std;
 
-int R, C, T, map[MAX][MAX], tmp[MAX][MAX], dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
-pair<pair<int, int>, int> air_cleaner;
+int R, C, T;
+int MAP[MAX][MAX];
+int C_MAP[MAX][MAX];
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+pair<int, int> Air_Cleaner[2];
+
+void map_copy(int A[][MAX], int B[][MAX]) {
+    for (int i = 0; i < R; i++)
+        for (int j = 0; j < C; j++)
+            A[i][j] = B[i][j];
+}
 
 int count_map() {
     int cnt = 0;
     for (int i = 0; i < R; i++)
         for (int j = 0; j < C; j++)
-            if (map[i][j] > 0)
-                cnt += map[i][j];
+            if (MAP[i][j] > 0)
+                cnt += MAP[i][j];
 
     return cnt;
 }
 
+
 void spread() {
-    for (int y = 0; y < R; y++) {
-        for (int x = 0; x < C; x++) {
-            if (map[y][x] > 0) { // 미세먼지
-                int cnt = 0;
-                int pol = map[y][x];
+    map_copy(C_MAP, MAP);
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            if (MAP[i][j] != 0 && MAP[i][j] != -1) {
+                int Cnt = 0;
+                int Value = MAP[i][j] / 5;
                 for (int k = 0; k < 4; k++) {
-                    int nx = x + dx[k];
-                    int ny = y + dy[k];
-                    if (nx < 0 || ny < 0 || nx >= C || ny >= R) continue;
-                    if (map[ny][nx] == -1) continue;
-                    cnt++;
-                    tmp[ny][nx] += (pol / 5);
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
+                        if (MAP[nx][ny] != -1) {
+                            C_MAP[nx][ny] = C_MAP[nx][ny] + Value;
+                            Cnt++;
+                        }
+                    }
                 }
-                tmp[y][x] += pol - ((pol / 5) * cnt);
+                C_MAP[i][j] = C_MAP[i][j] - (Cnt * Value);
             }
         }
     }
-    memcpy(map, tmp, sizeof(map));
-    memset(tmp, 0, sizeof(tmp));
+    map_copy(MAP, C_MAP);
 }
 
 void active_air_cleaner() {
-    int down = air_cleaner.first.first;
-    int up = air_cleaner.first.second;
-
-    for (int u = 0; u < 2; u++) {
-        if (u == 0) {
-            for (int i = up - 1; i > 0; i--)
-                map[i][0] = map[i - 1][0];
+    for (int Idx = 0; Idx < 2; Idx++) {
+        if (Idx == 0) {
+            for (int i = Air_Cleaner[Idx].first - 1; i > 0; i--)
+                MAP[i][0] = MAP[i - 1][0];
             for (int i = 0; i < C - 1; i++)
-                map[0][i] = map[0][i + 1];
-            for (int i = 1; i <= up; i++)
-                map[i - 1][C - 1] = map[i][C - 1];
+                MAP[0][i] = MAP[0][i + 1];
+            for (int i = 1; i <= Air_Cleaner[Idx].first; i++)
+                MAP[i - 1][C - 1] = MAP[i][C - 1];
             for (int i = C - 1; i > 1; i--)
-                map[up][i] = map[up][i - 1];
-            map[up][1] = 0;
+                MAP[Air_Cleaner[Idx].first][i] = MAP[Air_Cleaner[Idx].first][i - 1];
+            MAP[Air_Cleaner[Idx].first][1] = 0;
         } else {
-            for (int i = down + 1; i < R - 1; i++)
-                map[i][0] = map[i + 1][0];
+            for (int i = Air_Cleaner[Idx].first + 1; i < R - 1; i++)
+                MAP[i][0] = MAP[i + 1][0];
             for (int i = 0; i < C - 1; i++)
-                map[R - 1][i] = map[R - 1][i + 1];
-            for (int i = R - 1; i >= down; i--)
-                map[i][C - 1] = map[i - 1][C - 1];
+                MAP[R - 1][i] = MAP[R - 1][i + 1];
+            for (int i = R - 1; i >= Air_Cleaner[Idx].first; i--)
+                MAP[i][C - 1] = MAP[i - 1][C - 1];
             for (int i = C - 1; i > 1; i--)
-                map[down][i] = map[down][i - 1];
-
-            map[down][1] = 0;
+                MAP[Air_Cleaner[Idx].first][i] = MAP[Air_Cleaner[Idx].first][i - 1];
+            MAP[Air_Cleaner[Idx].first][1] = 0;
         }
     }
 }
@@ -78,17 +88,17 @@ int main() {
     cin.tie(nullptr);
 
     cin >> R >> C >> T;
-    for (int i = 0; i < R; i++)
+    int Idx = 0;
+    for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
-            cin >> map[i][j];
-            if (map[i][j] == -1) {
-                air_cleaner.second = j;
-                if (air_cleaner.first.first == 0)
-                    air_cleaner.first.first = i;
-                else
-                    air_cleaner.first.second = i;
+            cin >> MAP[i][j];
+            if (MAP[i][j] == -1) {
+                Air_Cleaner[Idx].first = i;
+                Air_Cleaner[Idx].second = j;
+                Idx++;
             }
         }
+    }
 
     while (T--) {
         spread();
