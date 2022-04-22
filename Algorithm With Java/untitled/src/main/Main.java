5220f42bde131fe393e778907dee0b94150dc30e
epicurean21 <jm_company@naver.com>
2022-04-22 16:46:15 +0900

    22/04/22 [1152 단더의 개수: 문자열] - JAVA

--- Algorithm With Java/untitled/src/main/Main.java
+++ Algorithm With Java/untitled/src/main/Main.java
@@ -6,27 +6,16 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder ans = new StringBuilder();
-
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int H = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
-            int N = Integer.parseInt(st.nextToken());
-
-            if (N <= H) {
-                ans.append(100 * N + 1).append('\n');
-            } else {
-                int count = 0;
-                while (N > H) {
-                    N -= H;
-                    count++;
-                }
-                ans.append(100 * N + (count + 1)).append('\n');
-            }
+        int count = 1;
+        String input = br.readLine();
+        for(int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == ' ')
+                count++;
         }
-
-        System.out.print(ans);
+        if(input.charAt(0) == ' ')
+            count--;
+        if(input.charAt(input.length() -1)  == ' ')
+            count--;
+        System.out.println(count);
     }
 }
