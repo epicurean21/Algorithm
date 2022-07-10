844ba70b80569a25919f964342ea7b533f57ee3c
epicurean21 <jm_company@naver.com>
2022-07-10 20:52:32 +0900

    22/07/10 [16936 나3곱2: 구현, 정렬]

--- Test.cpp
+++ Test.cpp
@@ -1,56 +1,16 @@
-/**
- * 1325 폴짝 폴짝
- * BFS, 그래프 탐색
- * ㅂㅅ같은 문제;;
- */
 #include <iostream>
-#include <vector>
-#include <queue>
 
 using namespace std;
-#define MAX 10002
-int N, a, b, steppingStone[MAX];
-bool visited[MAX];
-
-void bfs(int start) {
-    queue<pair<int, int>> q;
-    q.push({start, 0});
-    visited[start] = true;
-    while (!q.empty()) {
-        int cur = q.front().first;
-        int cnt = q.front().second;
-        q.pop();
-        if (cur == b) {
-            cout << cnt << "\n";
-            exit(0);
-        }
-        int moveSize = steppingStone[cur];
-        for (int i = 1; cur + (moveSize * i) <= N; i++) {
-            int nxt = cur + (moveSize * i);
-            if (!visited[nxt]) {
-                visited[nxt] = true;
-                q.push({nxt, cnt + 1});
-            }
-        }
-        for (int i = 1; cur - (moveSize * i) >= 1; i++) {
-            int nxt = cur - (moveSize * i);
-            if (!visited[nxt]) {
-                visited[nxt] = true;
-                q.push({nxt, cnt + 1});
-            }
-        }
-    }
-}
+#define MAX 1502
+int N, map[MAX][MAX];
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     cin >> N;
-    for (int i = 1; i <= N; i++) {
-        cin >> steppingStone[i];
-    }
-    cin >> a >> b;
-    bfs(a);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> map[i][j];
+
     return 0;
 }
