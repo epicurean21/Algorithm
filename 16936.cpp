61dd6b02228e36ad17ae20101747c02282212545
epicurean21 <jm_company@naver.com>
2022-07-17 23:54:59 +0900

    22/07/17 [16936 나3곱2: 구현, 수학, 정렬]

--- 16936.cpp
+++ 16936.cpp
@@ -1,6 +1,7 @@
 /**
  * 13936 나3곱2
- */
+ *//*
+
 #include <iostream>
 #include <vector>
 
@@ -64,6 +65,79 @@ int main() {
             } else if (Gob2(value, cur)) {
                 A_vector[j]->next = A_vector[i];
                 A_vector[i]->pre = A_vector[j];
+            }
+        }
+
+        if (A_vector[i]->pre == nullptr)
+            start = A_vector[i];
+    }
+    solve(start);
+
+    return 0;
+}*/
+
+/**
+* 16936 나3곱2
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define ll long long
+int N;
+ll a;
+
+class A {
+public:
+    ll value;
+    A *pre;
+    A *next;
+
+    A(ll v, A *p, A *n) {
+        this->value = v;
+        this->pre = p;
+        this->next = n;
+    }
+};
+
+bool Na3(ll x, ll y) {
+    return x / 3 == y;
+}
+
+bool Gob2(ll x, ll y) {
+    return x * 2 == y;
+}
+
+vector<A *> A_vector;
+A *start;
+
+void solve(A *aa) {
+    cout << aa->value << " ";
+    if (aa->next != nullptr)
+        solve(aa->next);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a;
+        A *value = new A(a, nullptr, nullptr);
+        A_vector.emplace_back(value);
+    }
+    for (int i = 0; i < N; i++) {
+        int cur = A_vector[i]->value;
+        for (int j = 0; j < N; j++) {
+            if (i == j) continue;
+            ll value = A_vector[j]->value;
+            if (cur % 3 == 0 && Na3(cur, value)) {
+                A_vector[i]->next = A_vector[j];
+                A_vector[j]->pre = A_vector[i];
+            } else if (Gob2(cur, value)) {
+                A_vector[i]->next = A_vector[j];
+                A_vector[j]->pre = A_vector[i];
             }
         }
 
