b46b7a09e8ad20468898e3e8247744d22c44a8ae
epicurean21 <jm_company@naver.com>
2022-09-01 13:31:41 +0900

    22/09/01 [14567 선수과목 : 위상정렬]

--- Test.cpp
+++ Test.cpp
@@ -1,44 +1,51 @@
+/**
+ * 14567 선수과목 (Prerequisite)
+ * 위상정렬
+ */
 #include <iostream>
-#include <stack>
 #include <vector>
+#include <queue>
 
 using namespace std;
-#define MAX 32001
-int N, M, A, B;
-bool visited[MAX];
-vector<vector<int>> map;
-stack<int> ans;
+#define MAX 1001
+int N, M, A, B, indeg[MAX], ans[MAX];
+queue<pair<int, int>> orders;
+vector<vector<int>> courses;
 
-void dfs(int cur) {
-    if (visited[cur])
-        return;
+void topological_sort() {
+    while (!orders.empty()) {
+        int cur = orders.front().first;
+        int semester = orders.front().second;
+        orders.pop();
+        ans[cur] = semester;
 
-    visited[cur] = true;
-    for (int i: map[cur])
-        if (!visited[i])
-            dfs(i);
-
-    ans.push(cur);
+        for (auto i: courses[cur]) {
+            indeg[i]--;
+            if (indeg[i] == 0) {
+                orders.push({i, semester + 1});
+            }
+        }
+    }
 }
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
     cin >> N >> M;
-    map.resize(N + 1);
+    courses.resize(N + 1);
     while (M--) {
         cin >> A >> B;
-        map[A].emplace_back(B);
+        courses[A].emplace_back(B);
+        indeg[B]++;
     }
 
     for (int i = 1; i <= N; i++)
-        if (!visited[i])
-            dfs(i);
+        if (indeg[i] == 0)
+            orders.push({i, 1});
 
-    while (!ans.empty()) {
-        cout << ans.top() << " ";
-        ans.pop();
-    }
-
+    topological_sort();
+    for (int i = 1; i <= N; i++)
+        cout << ans[i] << " ";
+    cout << '\n';
     return 0;
 }
