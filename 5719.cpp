2f48b25116a92cb86b809c6778575df3d27b35bb
epicurean21 <jm_company@naver.com>
2022-11-26 17:23:51 +0900

    22/11/26 [5719 거의 최단 경로: 그래프 탐색, 그래프 이론, 다익스트라, 너비우선탐색]

--- 5719.cpp
+++ 5719.cpp
@@ -1,79 +1,137 @@
+/**
+ * 5719 거의 최단 경로
+ * 그래프 탐색, 그래프 이론, 다익스트라, 너비우선탐색
+ *
+ * 거의최단경로란? -> 최단 경로에 포함되지 않는 엣지들만 이용한 경로 중 최단경로
+ * 즉, 최단 경로가 두 개가 있다면 해당되는 엣지들은 모두 사용 불가.
+ *
+ * How to solve?
+ * 1. Dijksta 1회 돌려서 최단 경로들을 찾는다.
+ * - 찾으면서 최단 경로의 반대 방향 edge를 생성한다.
+ * 2. 도착지로부터 BFS를 돌린다
+ * - 돌리면서 edge를 -1로 만든다 (즉, 최단경로는 사용하지 못하도록)
+ * 3. Dijkstra를 다시 돌린다.
+ */
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 
 using namespace std;
-#define MAX 501
+#define MAX 502
 int N, M, S, D, U, V, P;
+bool chk[MAX][MAX];
+vector<vector<pair<int, int>>> roads;
+vector<vector<pair<int, int>>> reversed_roads;
+vector<int> dist, ans;
 
-pair<int, int> map[MAX];
-vector<int> predecessor;
-
-int dijkstra(int s, int d) {
-    vector<int> final_distance;
-    vector<int> dist(N + 1, INT32_MAX);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    dist[s] = 0;
+void dijkstra_first(int s) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
     pq.push({0, s});
+    dist[s] = 0;
 
     while (!pq.empty()) {
         int cur = pq.top().second;
-        int cost = pq.top().first;
+        int cur_cost = pq.top().first;
         pq.pop();
 
-        if (dist[cur] < cost) continue;
+        if (dist[cur] < cur_cost) continue;
 
-        for (int i = 0; i < N; i++) {
-            if (map[cur][i].second == -1) continue;
-            int next = map[cur][i].first;
-            int nextCost = map[cur][i].second + cost;
+        for (auto n: roads[cur]) {
+            int next = n.first;
+            int next_cost = n.second + cur_cost;
 
-            if (dist[next] > nextCost) {
-                dist[next] = nextCost;
-                predecessor.clear();
-            } else if (dist[next] >= nextCost) {
-                predecessor
+            if (dist[next] > next_cost) {
+                dist[next] = next_cost;
+                pq.push({next_cost, next});
+                reversed_roads[next].clear();
+                reversed_roads[next].emplace_back(cur, n.second);
+            } else if (dist[next] == next_cost) {
+                reversed_roads[next].emplace_back(cur, n.second);
             }
         }
     }
-    return dist[d];
 }
 
-void remove_edges(int S, int D, int cur) {
-    if (now == S) return;
+void bfs(int d) {
+    queue<int> q;
+    q.push(d);
 
-    if (visited[now]) return;
-    visited[now] = true;
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
 
-    for (int i = 0; i < pre[now].size(); i++) {
-        int next = pre[now][i];
-        adj[next][now] = -1;
-        deletePath(pre[now][i]);
+        for (auto n: reversed_roads[cur]) {
+            int next = n.first;
+            if (chk[cur][next]) continue;
+            chk[cur][next] = true;
+
+            for (int i = 0; i < roads[next].size(); i++)
+                if (roads[next][i].first == cur)
+                    roads[next][i].second = -1;
+            q.push(next);
+        }
     }
 }
 
+void dijkstra_second(int s) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({0, s});
+    dist[s] = 0;
+
+    while (!pq.empty()) {
+        int cur = pq.top().second;
+        int cur_cost = pq.top().first;
+        pq.pop();
+
+        if (ans[cur] < cur_cost) continue;
+
+        for (auto n: roads[cur]) {
+            int next = n.first;
+            int next_cost = n.second;
+            if (next_cost == -1) continue;
+
+            if (ans[next] > next_cost + cur_cost) {
+                ans[next] = next_cost + cur_cost;
+                pq.push({next_cost + cur_cost, next});
+            }
+        }
+    }
+}
+
+void init(int n) {
+    roads.clear();
+    reversed_roads.clear();
+    dist.clear();
+    ans.clear();
+    roads.resize(n + 1);
+    reversed_roads.resize(n + 1);
+    dist.resize(n + 1, INT32_MAX);
+    ans.resize(n + 1, INT32_MAX);
+    memset(chk, false, sizeof(chk));
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr)->sync_with_stdio(false);
 
     while (cin >> N >> M) {
-        if (N == 0 && M == 0)
-            break;
-        map.clear();
-        predecessor.clear();
-        map.resize(N + 1);
+        if (N == 0 && M == 0) break;
+
+        init(N);
 
         cin >> S >> D;
         for (int i = 0; i < M; i++) {
             cin >> U >> V >> P;
-            map[U].emplace_back(V, P);
+            roads[U].emplace_back(V, P);
         }
 
-        int dist = dijkstra(S, D);
-        if (dist == -1) {
-            cout << "-1\n";
-            return 0;
-        }
+        dijkstra_first(S);
+        bfs(D);
+        dijkstra_second(S);
+
+        if (ans[D] == INT32_MAX) cout << "-1\n";
+        else cout << ans[D] << "\n";
     }
+
     return 0;
 }
--- Test.cpp
+++ Test.cpp
@@ -1,89 +1,137 @@
 /**
- * 20183 골목 대장 효석 - 효율성 2
- * 다익스트라, 그래프 이론
+ * 5719 거의 최단 경로
+ * 그래프 탐색, 그래프 이론, 다익스트라, 너비우선탐색
  *
- * 최단(최소비용) 경로를 찾으면서, 각 행성지이동 중 비용이 적은걸 우선으로 탐색.
- * 전체 비용도 계산해야한다.
+ * 거의최단경로란? -> 최단 경로에 포함되지 않는 엣지들만 이용한 경로 중 최단경로
+ * 즉, 최단 경로가 두 개가 있다면 해당되는 엣지들은 모두 사용 불가.
+ *
+ * How to solve?
+ * 1. Dijksta 1회 돌려서 최단 경로들을 찾는다.
+ * - 찾으면서 최단 경로의 반대 방향 edge를 생성한다.
+ * 2. 도착지로부터 BFS를 돌린다
+ * - 돌리면서 edge를 -1로 만든다 (즉, 최단경로는 사용하지 못하도록)
+ * 3. Dijkstra를 다시 돌린다.
  */
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 
 using namespace std;
-#define MAX 100001
-#define ll long long
-int N, M, A, B, C, a, b, c, cost[MAX]; // max_cost
-vector<vector<pair<int, int>>> map;
-priority_queue<pair<pair<ll, int>, int>> pq; // total_cost & max_cost & next
+#define MAX 502
+int N, M, S, D, U, V, P;
+bool chk[MAX][MAX];
+vector<vector<pair<int, int>>> roads;
+vector<vector<pair<int, int>>> reversed_roads;
+vector<int> dist, ans;
 
-int max(int x, int y) {
-    return x > y ? x : y;
-}
-
-void dijkstra() {
-    pq.push({{0, 0}, A});
-    cost[A] = 0;
+void dijkstra_first(int s) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({0, s});
+    dist[s] = 0;
 
     while (!pq.empty()) {
         int cur = pq.top().second;
-        ll total_cost = pq.top().first.first;
-        int max_cost = pq.top().first.second;
-
-
+        int cur_cost = pq.top().first;
         pq.pop();
 
-        if (cost[cur] < max_cost) return;
+        if (dist[cur] < cur_cost) continue;
 
-        cost[cur] = max_cost;
+        for (auto n: roads[cur]) {
+            int next = n.first;
+            int next_cost = n.second + cur_cost;
 
-        for (auto i: map[cur]) {
-            int next = i.first;
-            int next_cost = i.second;
-            ll next_total_cost = next_cost + total_cost;
-
-            if (next_total_cost <= C) {
-                pq.push({{next_total_cost, max(next_cost, max_cost)}, next});
+            if (dist[next] > next_cost) {
+                dist[next] = next_cost;
+                pq.push({next_cost, next});
+                reversed_roads[next].clear();
+                reversed_roads[next].emplace_back(cur, n.second);
+            } else if (dist[next] == next_cost) {
+                reversed_roads[next].emplace_back(cur, n.second);
             }
         }
     }
-
 }
 
-void init() {
-    map.resize(N + 1);
-    for (int i = 1; i <= N; i++)
-        cost[i] = INT32_MAX;
+void bfs(int d) {
+    queue<int> q;
+    q.push(d);
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for (auto n: reversed_roads[cur]) {
+            int next = n.first;
+            if (chk[cur][next]) continue;
+            chk[cur][next] = true;
+
+            for (int i = 0; i < roads[next].size(); i++)
+                if (roads[next][i].first == cur)
+                    roads[next][i].second = -1;
+            q.push(next);
+        }
+    }
+}
+
+void dijkstra_second(int s) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({0, s});
+    dist[s] = 0;
+
+    while (!pq.empty()) {
+        int cur = pq.top().second;
+        int cur_cost = pq.top().first;
+        pq.pop();
+
+        if (ans[cur] < cur_cost) continue;
+
+        for (auto n: roads[cur]) {
+            int next = n.first;
+            int next_cost = n.second;
+            if (next_cost == -1) continue;
+
+            if (ans[next] > next_cost + cur_cost) {
+                ans[next] = next_cost + cur_cost;
+                pq.push({next_cost + cur_cost, next});
+            }
+        }
+    }
+}
+
+void init(int n) {
+    roads.clear();
+    reversed_roads.clear();
+    dist.clear();
+    ans.clear();
+    roads.resize(n + 1);
+    reversed_roads.resize(n + 1);
+    dist.resize(n + 1, INT32_MAX);
+    ans.resize(n + 1, INT32_MAX);
+    memset(chk, false, sizeof(chk));
 }
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
-    cin >> N >> M >> A >> B >> C;
-    init();
+    while (cin >> N >> M) {
+        if (N == 0 && M == 0) break;
 
-    for (int i = 0; i < M; i++) {
-        cin >> a >> b >> c;
-        map[a].emplace_back(b, c);
-        map[b].emplace_back(a, c);
+        init(N);
+
+        cin >> S >> D;
+        for (int i = 0; i < M; i++) {
+            cin >> U >> V >> P;
+            roads[U].emplace_back(V, P);
+        }
+
+        dijkstra_first(S);
+        bfs(D);
+        dijkstra_second(S);
+
+        if (ans[D] == INT32_MAX) cout << "-1\n";
+        else cout << ans[D] << "\n";
     }
-
-    dijkstra();
-
-    if (cost[B] == INT32_MAX) cout << "-1\n";
-    else cout << cost[B] << "\n";
-
 
     return 0;
 }
-
-/*
-6 8 1 6 20
-1 2 7
-1 4 3
-2 4 1
-2 5 3
-4 3 3
-3 6 8
-3 5 2
-5 6 4
-*/
