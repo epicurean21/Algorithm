445e142b5fdcdc242e9b614c6406e7de20728f84
epicurean21 <jm_company@naver.com>
2023-03-21 23:16:25 +0900

    23/03/21 [2812 크게 만들기: 덱, 자료구조]

--- Test.cpp
+++ Test.cpp
@@ -1,82 +1,44 @@
+/**
+ * 2812 크게 만들기
+ * 스택, 자료구조, 덱
+ */
 #include "iostream"
-#include "vector"
-#include "algorithm"
+#include "deque"
 
 using namespace std;
-#define MAX 101
-int r, c, k, map[MAX][MAX], cnt, row, col, currentLength;
 
-void calculateR() {
-    int maxLength = 0;
-    for (int i = 0; i < col; i++) {
-        vector<pair<int, int>> calculateNumbers;
-        vector<int> countNumbers(MAX);
-
-        for (int j = 0; j < row; j++) countNumbers[map[i][j]]++;
-
-        for (int j = 1; j < MAX; j++)
-            if (countNumbers[j] != 0) calculateNumbers.emplace_back(countNumbers[j], j);
-        sort(calculateNumbers.begin(), calculateNumbers.end());
-
-        currentLength = 0;
-        for (int j = 0; j < row; j++) map[i][j] = 0;
-        for (int j = 0; j < calculateNumbers.size(); j++) {
-            map[i][currentLength++] = calculateNumbers[j].second;
-            map[i][currentLength++] = calculateNumbers[j].first;
-        }
-
-        maxLength = max(maxLength, currentLength);
-    }
-    row = maxLength;
-}
-
-void calculateC() {
-    int maxLength = 0;
-    for (int i = 0; i < row; i++) {
-        vector<pair<int, int>> calculateNumbers;
-        vector<int> countNumbers(MAX);
-
-        for (int j = 0; j < col; j++) countNumbers[map[j][i]]++;
-
-        for (int j = 1; j < MAX; j++)
-            if (countNumbers[j] != 0) calculateNumbers.emplace_back(countNumbers[j], j);
-        sort(calculateNumbers.begin(), calculateNumbers.end());
-
-        currentLength = 0;
-        for (int j = 0; j < col; j++) map[j][i] = 0;
-        for (int j = 0; j < calculateNumbers.size(); j++) {
-            map[currentLength++][i] = calculateNumbers[j].second;
-            map[currentLength++][i] = calculateNumbers[j].first;
-        }
-
-        maxLength = max(maxLength, currentLength);
-    }
-    col = maxLength;
-}
-
-void init() {
-    r--;
-    c--;
-    col = row = 3;
-}
+int n, k, cnt;
+string input;
+deque<char> dq;
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
-    cin >> c >> r >> k;
-    init();
+    cin >> n >> k;
+    cin >> input;
 
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            cin >> map[i][j];
-
-    while (map[c][r] != k && ++cnt <= 100) {
-        if (col >= row) calculateR();
-        else calculateC();
+    int i;
+    for (i = 0; i < input.length(); i++) {
+        if (cnt == k) break;
+        while (!dq.empty() && dq.back() - '0' < input[i] - '0' && cnt < k) {
+            dq.pop_back();
+            cnt++;
+        }
+        dq.push_back(input[i]);
     }
 
-    if (map[c][r] != k || 100 < cnt) cout << "-1\n";
-    else cout << cnt << '\n';
+    for (; i < input.length(); i++) dq.push_back(input[i]);
+    while (cnt < k) {
+        dq.pop_back();
+        cnt++;
+    }
+
+    while (!dq.empty()) {
+        cout << dq.front();
+        dq.pop_front();
+        cnt++;
+    }
+    cout << '\n';
 
     return 0;
 }
