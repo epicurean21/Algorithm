9e8fc4c3ad3b5e0cc6421c33b9917bcfd15b56ad
epicurean21 <jm_company@naver.com>
2023-03-25 23:47:15 +0900

    23/03/25 [leetCode #14 Longest Common Prefix: String, Math]

--- Test.cpp
+++ Test.cpp
@@ -1,63 +1,44 @@
 /**
- * leetcode #13
- * Roman to Integer
+ * leetcode #14
+ * Longest Common Prefix
  * -
- * translate roman numerals to an integer
+ * get longest common prefix
+ * if there is no common prefix, return ""
+ * Solution:
+ * just compare between the shortest string and the longest string.
+ * find the common prefix between two strings.
  */
 #include "iostream"
-#include "stack"
-
+#include "vector"
 using namespace std;
-int romanNumerals[26];
 
-void init() {
-    romanNumerals['I' - 'A'] = 1;
-    romanNumerals['V' - 'A'] = 5;
-    romanNumerals['X' - 'A'] = 10;
-    romanNumerals['L' - 'A'] = 50;
-    romanNumerals['C' - 'A'] = 100;
-    romanNumerals['D' - 'A'] = 500;
-    romanNumerals['M' - 'A'] = 1000;
-}
-
-int getRomanCombinationNumber(string roman) {
-    if (roman == "IV") return 4;
-    else if (roman == "IX") return 9;
-    else if (roman == "XL") return 40;
-    else if (roman == "XC") return 90;
-    else if (roman == "CD") return 400;
-    else if (roman == "CM") return 900;
-    else return 0;
-}
-
-int romanToInt(string s) {
-    int calculatedNumber = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'I' || s[i] == 'X' || s[i] == 'C') {
-            string combinationString;
-            combinationString += s[i];
-            if (i != s.length()) combinationString += s[i + 1];
-
-            int romanCombinationNumber = getRomanCombinationNumber(combinationString);
-            if (romanCombinationNumber) {
-                calculatedNumber += romanCombinationNumber;
-                i++;
-            } else calculatedNumber += romanNumerals[s[i] - 'A'];
-        } else calculatedNumber += romanNumerals[s[i] - 'A'];
+class Solution {
+private:
+    int strsSize = 0;
+    string n, m, ans;
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        sort(strs.begin(), strs.end());
+        strsSize = strs.size();
+        n = strs[0];
+        m = strs[strsSize - 1];
+        ans = "";
+        for (int i = 0; i < n.size(); i++) {
+            if (n[i] == m[i]) { ans += n[i]; }
+            else break;
+        }
+        return ans;
     }
-
-    return calculatedNumber;
-}
+};
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
-    init();
-    string s;
-    while (cin >> s) {
-        if (s == "0") exit(1);
-        cout << romanToInt(s) << '\n';
-    }
+    Solution solution;
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << solution.longestCommonPrefix(strs) << "\n";
+    strs = {"dog", "racecar", "car"};
+    cout << solution.longestCommonPrefix(strs) << "\n";
 
     return 0;
 }
