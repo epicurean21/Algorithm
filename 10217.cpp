7f8be986e88c2afbacd832bc491282692b8223fd
epicurean21 <jm_company@naver.com>
2023-06-23 18:33:17 +0900

    23/06/23 [10217 KCM Travel: Dijkstra]

---
+++ 10217.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+//앞에는 다음 공항, 뒤는 cost
+vector<pair<int, pair<int, int> > > idx[101];
+const int INF = 1000000007;
+int timeDelay[101][10001];
+
+void dijkstra(const int &N, const int &MAX_COST) {
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= MAX_COST; j++)
+            timeDelay[i][j] = INF;
+    }
+    timeDelay[1][0] = 0;
+    priority_queue<pair<int, pair<int, int> > > pq;
+    pq.push({0, {0, 1}});
+    while (!pq.empty()) {
+        int cost = -pq.top().first;
+        int time = pq.top().second.first;
+        int here = pq.top().second.second;
+        pq.pop();
+        if (timeDelay[here][cost] < time)
+            continue;
+        for (int i = 0; i < idx[here].size(); i++) {
+            int nextCost = cost + idx[here][i].second.first;
+            int there = idx[here][i].first;
+            int nextTime = time + idx[here][i].second.second;
+
+            //만일 최대 비용을 초과하는 가격이 나온다면, 패스한다.
+            if (nextCost > MAX_COST)
+                continue;
+            //이 비용 위로 이 시간이 최소 시간이 되는지 확인하고, 미리 설정해준다.
+            //이유는, pq에 push를 최소로 하도록 만들어주고, 이미 들어간 값들의 무의미한 계산을 막아주기 위해서이다.
+            if (timeDelay[there][nextCost] > nextTime) {
+                for (int i = nextCost; i <= MAX_COST; i++) {
+                    if (timeDelay[there][i] > nextTime)
+                        timeDelay[there][i] = nextTime;
+                }
+                pq.push({-nextCost, {nextTime, there}});
+            }
+        }
+    }
+    int ret = INF;
+    for (int i = 1; i <= MAX_COST; i++)
+        ret = min(ret, timeDelay[N][i]);
+    if (ret == INF)
+        cout << "Poor KCM" << endl;
+    else
+        cout << ret << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //T=테스트 케이스 수, N=공항의 수, M=지원 비용, K=티켓정보의 수
+    int T, N, M, K;
+    //u=출발공항, v=도착공항, c=비용, d=소요시간
+    int u, v, c, d;
+    cin >> T;
+    while (T--) {
+        cin >> N >> M >> K;
+        for (int i = 1; i <= N; i++) {
+            while (!idx[i].empty())
+                idx[i].pop_back();
+        }
+        for (int i = 0; i < K; i++) {
+            cin >> u >> v >> c >> d;
+            idx[u].push_back({v, {c, d}});
+        }
+        dijkstra(N, M);
+    }
+
+
+    return 0;
+}
--- Test.cpp
+++ Test.cpp
@@ -1,109 +1,27 @@
-#include <stdio.h>
-#include <memory.h>
-#include <time.h>
+/**
+ * 2820 자동차 공장
+ * 자료구조, 세그먼트 트리, Lazy Propagation
+ *
+ */
 
-extern void rect_fill(int x1, int y1, int x2, int y2);
+#include "iostream"
+#include "cmath"
+#include "vector"
 
-extern void rect_clear(int x1, int y1, int x2, int y2);
-
-extern void rect_reverse(int x1, int y1, int x2, int y2);
-
-extern unsigned char *get_vram();
-
-unsigned char VRAM[1024 * 8192];
-
-long long RSEED = 55, TRY = 256;
-
-unsigned int random() {
-    RSEED = RSEED * 25214903917ULL + 11ULL;
-    return (RSEED >> 16) & 0x7FFFFFFF;
-}
-
-static void _set_pixel(int x1, int y1) {
-    int xbase = x1 / 8, xoffset = x1 % 8;
-    int y = (y1 % 4) * 4 + y1 / 4;
-
-    VRAM[y * xbase] |= (0x80 >> xoffset);
-}
-
-static void _reset_pixel(int x1, int y1) {
-    int xbase = x1 / 8, xoffset = x1 % 8;
-    int y = (y1 % 4) * 4 + y1 / 4;
-
-    VRAM[y * xbase] &= ~(0x80 >> xoffset);
-}
-
-static int _get_pixel(int x1, int y1) {
-    int xbase = x1 / 8, xoffset = x1 % 8;
-    int y = (y1 % 4) * 4 + y1 / 4;
-
-    return VRAM[y * xbase] & (0x80 >> xoffset);
-}
-
-static void _rect_fill(int x1, int y1, int x2, int y2) {
-    for (int y = y1; y <= y2; y++)
-        for (int x = x1; x <= x2; x++)
-            _set_pixel(x, y);
-}
-
-
-static void _rect_clear(int x1, int y1, int x2, int y2) {
-    for (int y = y1; y <= y2; y++)
-        for (int x = x1; x <= x2; x++)
-            _reset_pixel(x, y);
-}
-
-static void _rect_reverse(int x1, int y1, int x2, int y2) {
-    for (int y = y1; y <= y2; y++)
-        for (int x = x1; x <= x2; x++)
-            if (_get_pixel(x, y) == 0)
-                _set_pixel(x, y);
-            else
-                _reset_pixel(x, y);
-}
+using namespace std;
+int n, m;
+vector<int> segment_tree, lazy_propagation_tree, ;
 
 int main() {
-    long long SCORE = 0;
+    cin.tie(nullptr)->sync_with_stdio(false);
 
-    RSEED = 55;
-    for (int i = 0; i < TRY; i++) {
-        int type = random() % 3;
-        int x1 = random() % 8192;
-        int y1 = random() % 8192;
-        int x2 = random() % 8192;
-        int y2 = random() % 8192;
+    cin >> n >> m;
+    int h = (int) ceil(log2(n + 1));
+    segment_tree.resize(1 << (h + 1));
+    lazy_propagation_tree.resize(1 << (h + 1));
 
-        if (x1 > x2 || y1 > y2) continue;
-
-        if (type == 0) _rect_fill(x1, y1, x2, y2);
-        else if (type == 1) _rect_clear(x1, y1, x2, y2);
-        else _rect_reverse(x1, y1, x2, y2);
+    for (int i = 0; i < n; i++) {
 
     }
-    RSEED = 55;
-    time_t TIME = clock();
-    for (int i = 0; i < TRY; i++) {
-        int type = random() % 3;
-        int x1 = random() % 8192;
-        int y1 = random() % 8192;
-        int x2 = random() % 8192;
-        int y2 = random() % 8192;
-
-        if (x1 > x2 || y1 > y2) continue;
-
-        if (type == 0) rect_fill(x1, y1, x2, y2);
-        else if (type == 1) rect_clear(x1, y1, x2, y2);
-        else rect_reverse(x1, y1, x2, y2);
-
-    }
-    unsigned char *vram = get_vram();
-
-    SCORE += (clock() - TIME) / (CLOCKS_PER_SEC / 1000);
-
-    if (memcmp(vram, VRAM, 1024 * 8192) != 0) {
-        SCORE += 100000000;
-    }
-
-    printf("SCORE : %lld\n", SCORE);
     return 0;
 }
