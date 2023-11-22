843aecf8f68df93f2e27445604c3b69cf9794939
epicurean21 <jm_company@naver.com>
2023-11-23 00:29:42 +0900

    23/11/23 [11060 점프 점프 : 다이나믹 프로그래밍]

--- 11060-dp.cpp
+++ 11060-dp.cpp
@@ -1,8 +1,13 @@
+/**
+ * 11060 점프점프
+ * 다이나믹 프로그래밍
+ */
 #include "iostream"
 
 using namespace std;
 #define MAX 1001
 int n, a[MAX], dp[MAX];
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
@@ -14,7 +19,8 @@ int main() {
         if (dp[i] == 0) continue;
 
         for (int j = 1; j <= a[i] && (i + j < MAX); j++)
-            if (dp[i + j] > dp[i] + 1 || dp[i + j] == 0) dp[i + j] = dp[i] + 1;
+            if (dp[i + j] > dp[i] + 1 || dp[i + j] == 0) // 이동 가능한 위치의 수가 아직 0 이거나, 현재 위치보다 많이 움직여야 하면
+                dp[i + j] = dp[i] + 1; // 현재 위치 + 1 로 최적화
     }
     cout << (dp[n - 1] == 0 ? -1 : dp[n - 1] - 1) << '\n';
     return 0;
