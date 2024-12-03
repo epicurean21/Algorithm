3d10f24974619f28bc41b536f0dc60ea357900fd
epicurean21 <jm_company@naver.com>
2024-12-03 22:31:03 +0900

    24/12/03 [16235 나무 재태크: 구현, 시뮬레이션, 자료구조]

--- Test.cpp
+++ Test.cpp
@@ -1,72 +1,114 @@
 #include "bits/stdc++.h"
 
 using namespace std;
+#define MAX 1001
+int N, M, K, A[MAX][MAX], x, y, z, field[MAX][MAX],
+        dx[] = {1, 1, 0, -1, -1, -1, 0, 1},
+        dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+vector <pair<int, pair < int, int>>>
+trees;
+vector <pair<int, pair < int, int>>>
+dead_trees;
 
-#define ERROR "Error!\n"
-#define UNDER_SCORE '_'
-string input, result;
-stack<char> st;
+void spring() {
+    sort(trees.begin(), trees.end());
 
-bool isCapitalLetter(char c) {
-    return 'A' <= c && c <= 'Z';
+    for (int i = 0; i < trees.size(); i++) {
+        int age = trees[i].first;
+        int c = trees[i].second.first;
+        int r = trees[i].second.second;
+
+        if (field[r][c] < age) {
+            dead_trees.push_back(trees[i]);
+        } else {
+            field[r][c] -= age;
+            trees[i].first += 1;
+        }
+    }
+
+    vector < pair < int, pair < int, int>>> temp;
+    for (int i = 0; i < trees.size(); i++) {
+        if (trees[i].first != -1) {
+            temp.push_back(trees[i]);
+        }
+    }
+
+    trees = temp;
 }
 
-char convertCharacter(bool is_capital, char c) {
-    if (is_capital) return (c - 'A' + 'a');
-    else return (c - 'a' + 'A');
+void summer() {
+    if (dead_trees.empty()) {
+        return;
+    }
+    for (int i = 0; i < dead_trees.size(); i++) {
+        int age = dead_trees[i].first;
+        int position_x = dead_trees[i].second.first;
+        int position_y = dead_trees[i].second.second;
+
+        int nutrient = age / 2;
+        A[position_y][position_x] += nutrient;
+    }
+    dead_trees.clear();
+}
+
+void autumn() {
+    int trees_count = trees.size();
+    for (int i = 0; i < trees_count; i++) {
+        int age = trees[i].first;
+        if (age % 5) {
+            continue;
+        }
+        int position_x = trees[i].second.first;
+        int position_y = trees[i].second.second;
+
+        for (int k = 0; k < 8; k++) {
+            int next_x = position_x + dx[k];
+            int next_y = position_y + dy[k];
+
+            if (next_x < 1 || next_x > N || next_y < 1 || next_y > N) continue;
+
+            trees.push_back({1, {next_x, next_y}});
+        }
+    }
+}
+
+void winter() {
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++)
+            field[i][j] += A[i][j];
+}
+
+void print_tree_list() {
+    for (int i = 0; i < trees.size(); i++) {
+        cout << "x: " << trees[i].second.first << ", y: " << trees[i].second.second << ", age: " << trees[i].first
+             << '\n';
+    }
 }
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
-    cin >> input;
-    if (input[0] == UNDER_SCORE || input[input.length() - 1] == UNDER_SCORE) {
-        cout << ERROR;
-        return 0;
+    cin >> N >> M >> K;
+
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++) {
+            field[i][j] = 5;
+            cin >> A[i][j];
+        }
+
+    for (int i = 0; i < M; i++) {
+        cin >> y >> x >> z;
+        trees.push_back({z, {x, y}});
     }
 
-    if (input.find(UNDER_SCORE) == string::npos && isCapitalLetter(input[0])) {
-        cout << ERROR;
-        return 0;
+    while (K--) {
+        if (trees.empty()) break;
+        spring();
+        summer();
+        autumn();
+        winter();
     }
 
-    bool is_java = false, is_c = false;
-    for (char c: input) {
-        if (!st.empty() && st.top() == UNDER_SCORE && c == UNDER_SCORE) {
-            cout << ERROR;
-            return 0;
-        }
-        if (c == UNDER_SCORE) {
-            if (is_java) {
-                cout << ERROR;
-                return 0;
-            }
-            is_c = true;
-            st.push(c);
-            continue;
-        }
-
-        if (isCapitalLetter(c)) {
-            if (is_c) {
-                cout << ERROR;
-                return 0;
-            }
-            is_java = true;
-            st.push(c);
-            result += UNDER_SCORE;
-            result += convertCharacter(true, c);
-            continue;
-        }
-
-        if (!st.empty() && st.top() == UNDER_SCORE) {
-            result += convertCharacter(false, c);
-            st.push(c);
-            continue;
-        }
-
-        result += c;
-    }
-    cout << result << '\n';
-
+    cout << trees.size() << '\n';
     return 0;
 }
